798499bdcb87486334f4ef115b7b988db6a74ca7
Sujal Lothe <130586852+Sujal369@users.noreply.github.com>
2024-02-26 23:56:33 +0530

    Update MissingNumber.java

--- Arrray/MissingNumber.java
+++ Arrray/MissingNumber.java
@@ -1,11 +1,23 @@
-class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums); 
-        for (int i = 0; i < nums.length - 1; i++) {
-                if (nums[i] == nums[i+1]) {
-                    return true;
-                }
+import java.util.Arrays;
+
+class MissingNumber
+{
+    public static int missNumber(int[] arr){
+        int n =  arr.length;
+        int[] v = new int[n+1];
+        Arrays.fill(v,-1);
+        for (int i = 0;i<n;i++){
+            v[arr[i]] = arr[i];
         }
-        return false;
+        for (int i=0;i<v.length;i++){
+            if(v[i] == -1){ return i;}
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {0,1,3};
+
+        System.out.print(missNumber(arr));
     }
 }
