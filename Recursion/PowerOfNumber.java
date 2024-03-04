e42d3fffca5c8e7e10ef58d0c81ff54fc20de057
Sujal Lothe <130586852+Sujal369@users.noreply.github.com>
2024-03-04 22:06:31 +0530

    Add files via upload

---
+++ Recursion/PowerOfNumber.java
@@ -0,0 +1,21 @@
+class PowerOfNumber
+{
+    public static void main(String[] args) {
+
+        int base = 3, powerRaised = 4;
+        int result = power(base, powerRaised);
+
+        System.out.println(base + "^" + powerRaised + "=" + result);
+    }
+
+    public static int power(int base, int powerRaised) {
+        if (powerRaised != 0) {
+
+            // recursive call to power()
+            return (base * power(base, powerRaised - 1));
+        }
+        else {
+            return 1;
+        }
+    }
+}
